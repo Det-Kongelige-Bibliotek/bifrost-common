62e47da4f6bb053b04bf61081779e6ef4920bb42
Ronan McHugh <mchugh.r@gmail.com>
2014-06-03 14:08:53 +0200

    Generator copies schema to solr_conf dir

--- lib/generators/bifrost/install_generator.rb
+++ lib/generators/bifrost/install_generator.rb
@@ -1,8 +1,13 @@
 require 'rails/generators'
 module Bifrost
   class InstallGenerator < Rails::Generators::Base
+
+    source_root File.expand_path('../templates', __FILE__)
+
     desc 'Install Bifrost specific logic'
     def run_install
+      puts "Replacing default solr schema with Bifrost version"
+      copy_file 'schema.xml', 'solr_conf/conf/schema.xml', force: true
     end
   end
 end
